f8acd01b4a076ede303052c40dfdec8f6fb61b27
IlyaSaltanov <mail@saltanov-pro.ru>
2025-10-21 23:16:54 +0500

    Bit Counting

---
+++ C#/Bit Counting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Kata
+{
+    public static int CountBits(int n)
+    {
+      int count = 0;
+      string str = Convert.ToString(n, 2);
+      foreach (char a in str)
+        {
+        if (a == '1')
+          {
+          count++;
+        }
+      }
+    return count;
+    }
+}
