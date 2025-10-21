2ac4b7869d041034c3c20f6f8a6fb34b6f787a51
IlyaSaltanov <mail@saltanov-pro.ru>
2025-10-21 22:57:46 +0500

    Multiples of 3 or 5

---
+++ C#/Multiples of 3 or 5/Program.cs
@@ -0,0 +1,15 @@
+﻿public static class Kata
+{
+  public static int Solution(int value)
+  {
+    int count = 0;
+    for (int i = 0; i < value; i++)
+      {
+      if (i % 3 == 0 || i % 5 == 0)
+        {
+        count = count + i;
+      }
+    }
+    return count;
+  }
+}
