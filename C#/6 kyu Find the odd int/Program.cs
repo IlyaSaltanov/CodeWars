438d7abf5455c698e8a6941c69ac0e0b86416d78
IlyaSaltanov <mail@saltanov-pro.ru>
2025-10-22 11:32:59 +0500

    6 kyu Find the odd int

---
+++ C#/6 kyu Find the odd int/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Solution
+{
+  class Kata
+    {
+    public static int find_it(int[] seq) 
+      {
+      for (int i = 0; i < seq.Length; i++)
+        {
+          int k = 0;
+          var variable = seq[i];
+          for (int q = 0; q < seq.Length; q++)
+          {
+            if (variable == seq[q])
+            {
+              k++;
+            }
+          }
+          if (k % 2 == 1)
+          {
+            return variable;
+          }
+        }
+      return 0;
+      }  
+    }
+}
