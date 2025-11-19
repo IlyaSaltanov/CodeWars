bf7c403f1bb7f79668f6e929eed952ba14de6cd3
IlyaSaltanov <mail@saltanov-pro.ru>
2025-11-19 08:57:29 +0500

    commit

--- C#/Multiples of 3 or 5/obj/Debug/net9.0/Multiples of 3 or 5.AssemblyInfo.cs
+++ C#/Multiples of 3 or 5/obj/Debug/net9.0/Multiples of 3 or 5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Multiples of 3 or 5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+396a1c0a3b1392884c18696b5b54ff53bef41428")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e78a268a988447318efc396db1b88afe70b26030")]
 [assembly: System.Reflection.AssemblyProductAttribute("Multiples of 3 or 5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Multiples of 3 or 5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
