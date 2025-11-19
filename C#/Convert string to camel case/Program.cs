707504bdadbde96a43efefc594adad5f5c0c855d
IlyaSaltanov <mail@saltanov-pro.ru>
2025-11-19 08:56:42 +0500

    Convert string to camel case

---
+++ C#/Convert string to camel case/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Kata
+{
+  public static string ToCamelCase(string str)
+  {
+    string[] sp = str.Split(new char[] {'-', '_'});
+    for (int i = 1; i < sp.Length; i++)
+    {
+       sp[i] = char.ToUpper(sp[i][0]) + sp[i].Substring(1);
+    }
+    return string.Join("",sp);
+  }
+}
