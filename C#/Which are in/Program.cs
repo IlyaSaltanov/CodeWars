405560b9ca162e2c787e1fea4532e8f2501261e7
IlyaSaltanov <mail@saltanov-pro.ru>
2025-11-21 08:52:40 +0500

    solved task Which

---
+++ C#/Which are in/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+class WhichAreIn
+{
+    public static string[] inArray(string[] array1, string[] array2)
+    {
+        //       string[] r = new string[array1.Length];
+        List<string> r = new List<string>();
+        for (int i = 0; i < array1.Length; i++)
+        {
+            string word = array1[i];
+            for (int q = 0; q < array2.Length; q++)
+            {
+                string fullWord = array2[q];
+                if (fullWord.Contains(word))
+                {
+                    //             r[i] = word;
+                    r.Add(word);
+                    break;
+                }
+            }
+        }
+        //     r = r.OrderBy(w => w).ToArray();
+        //     return r;
+        return r.OrderBy(w => w).ToArray();
+    }
+}
+
+
