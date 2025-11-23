9bce4ed59de13ea4ca7a0b7729cdc14221a502f0
IlyaSaltanov <mail@saltanov-pro.ru>
2025-11-23 10:15:19 +0500

    solved Moving Zeros To The End

---
+++ C#/Moving Zeros To The End/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Kata
+{
+  public static int[] MoveZeroes(int[] arr)
+  {
+    int col = arr.Length * 2;
+    while (col != 0)
+    {
+      for (int i = 0; i < arr.Length - 1; i++)
+      {
+        if (arr[i] == 0)
+        {
+          var nextNumbre = arr[i+1];
+          var nole = arr[i];
+          arr[i+1] = nole;
+          arr[i] = nextNumbre;
+        }
+      }
+      col -= 1;
+    }
+    return arr;
+  }
+}
