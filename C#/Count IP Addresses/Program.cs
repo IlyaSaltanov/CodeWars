3048ead35c0fd4d4fa76e2c0d07f2839144133c1
IlyaSaltanov <mail@saltanov-pro.ru>
2025-11-23 12:31:44 +0500

    solved Count IP Addresses

---
+++ C#/Count IP Addresses/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class CountIPAddresses
+{
+   public static long IpsBetween(string start, string end)
+   {
+      var spStart = start.Split('.');
+      var spEnd = end.Split('.');
+      
+      int[] spIntStart = new int[spStart.Length];
+      int[] spIntEnd = new int[spEnd.Length];
+     
+      for (int q = 0; q < spEnd.Length; q++)
+      {
+        spIntStart[q] = int.Parse(spStart[q]);
+        spIntEnd[q] = int.Parse(spEnd[q]);
+      }
+      
+      long s1 = 0;
+      long s2 = 0;
+      s1 = (spIntStart[0] * (long)Math.Pow(256, 3)) + ((spIntStart[1] * (long)Math.Pow(256, 2))) + ((spIntStart[2] * (long)Math.Pow(256, 1))) + spIntStart[3];
+      s2 = (spIntEnd[0] * (long)Math.Pow(256, 3)) + ((spIntEnd[1] * (long)Math.Pow(256, 2))) + ((spIntEnd[2] * (long)Math.Pow(256, 1))) + spIntEnd[3];
+     
+      return s2 - s1;
+   }
+}
---
+++ C#/ForTests/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(10%(10+1));
+    }
+}
+
+// Дополнительные элементы можно добавить здесь
